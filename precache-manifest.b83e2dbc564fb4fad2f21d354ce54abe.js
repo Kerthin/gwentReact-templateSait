9e6684ce1190d6bef824f888b3f257e1d28b35cc
Kerthin <belichenko.roden@list.ru>
2021-03-09 14:56:24 +0300

    Updates

---
+++ precache-manifest.b83e2dbc564fb4fad2f21d354ce54abe.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a737285e9173b933b6bfb115c91755b0",
+    "url": "/gwentReact-templateSait/index.html"
+  },
+  {
+    "revision": "bc77ef8f38a1e0eb2b25",
+    "url": "/gwentReact-templateSait/static/css/main.8df975b5.chunk.css"
+  },
+  {
+    "revision": "84caae8ad1efb4edd1ff",
+    "url": "/gwentReact-templateSait/static/js/2.3241f22d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gwentReact-templateSait/static/js/2.3241f22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc77ef8f38a1e0eb2b25",
+    "url": "/gwentReact-templateSait/static/js/main.76fe2c1f.chunk.js"
+  },
+  {
+    "revision": "1e9dff479e5637e3706b",
+    "url": "/gwentReact-templateSait/static/js/runtime-main.878ea44b.js"
+  },
+  {
+    "revision": "38df19dca34b3bde75c7a8e51c42ce6c",
+    "url": "/gwentReact-templateSait/static/media/bg.38df19dc.jpg"
+  },
+  {
+    "revision": "197ae5344c83cec582387cf6786c3316",
+    "url": "/gwentReact-templateSait/static/media/bloodScent.197ae534.png"
+  },
+  {
+    "revision": "48783bbaabe6d2f46838c6358638ded8",
+    "url": "/gwentReact-templateSait/static/media/border.48783bba.png"
+  },
+  {
+    "revision": "691f3752a3ccf6885f93b1e75eb3e84a",
+    "url": "/gwentReact-templateSait/static/media/border.691f3752.png"
+  },
+  {
+    "revision": "9101c4c12f66f57ad19a6c13b61ed932",
+    "url": "/gwentReact-templateSait/static/media/borderBronze.9101c4c1.png"
+  },
+  {
+    "revision": "87df6db787540d9eed4699d393052aae",
+    "url": "/gwentReact-templateSait/static/media/borderGold.87df6db7.png"
+  },
+  {
+    "revision": "bfe75e9e36c3a392c55a0927e3b7f615",
+    "url": "/gwentReact-templateSait/static/media/center.bfe75e9e.jpg"
+  },
+  {
+    "revision": "1afa379d5051f8f0bdfa51554acba6c4",
+    "url": "/gwentReact-templateSait/static/media/clock.1afa379d.png"
+  },
+  {
+    "revision": "150f7787e80b18790304ebe3576cc7d3",
+    "url": "/gwentReact-templateSait/static/media/deadeyeAmbush.150f7787.png"
+  },
+  {
+    "revision": "7515a9e159f02375683132510c0c78a9",
+    "url": "/gwentReact-templateSait/static/media/deathShadow.7515a9e1.png"
+  },
+  {
+    "revision": "d10bbf4d280ca8ee0c2445d11c4c17b4",
+    "url": "/gwentReact-templateSait/static/media/enslave.d10bbf4d.png"
+  },
+  {
+    "revision": "10d6b4481d100b3b4282dcd2c33da7dd",
+    "url": "/gwentReact-templateSait/static/media/foltest.10d6b448.png"
+  },
+  {
+    "revision": "d6ac83d73d48710c517177622875e413",
+    "url": "/gwentReact-templateSait/static/media/fruitsYsgith.d6ac83d7.png"
+  },
+  {
+    "revision": "2279173225fbd295b959025913a2cd06",
+    "url": "/gwentReact-templateSait/static/media/gwent.22791732.ttf"
+  },
+  {
+    "revision": "9612062318cfac7f28dec6d3d9023447",
+    "url": "/gwentReact-templateSait/static/media/hiddenCache.96120623.png"
+  },
+  {
+    "revision": "57ef2c8ba9f059cb298623acaf4824be",
+    "url": "/gwentReact-templateSait/static/media/imperialFormation.57ef2c8b.png"
+  },
+  {
+    "revision": "9cb3b0fd03e8508c8793c396349ce52d",
+    "url": "/gwentReact-templateSait/static/media/mysticEcho.9cb3b0fd.png"
+  },
+  {
+    "revision": "63f36d0f66ed1fe8d038381e043af79f",
+    "url": "/gwentReact-templateSait/static/media/pincerManeuver.63f36d0f.png"
+  },
+  {
+    "revision": "55f6f03e12b5219403a4f853c9c81241",
+    "url": "/gwentReact-templateSait/static/media/precisionStrike.55f6f03e.png"
+  },
+  {
+    "revision": "3c1b9f1a920658faefc85a98e736ceb2",
+    "url": "/gwentReact-templateSait/static/media/preloader1280x960.3c1b9f1a.jpg"
+  },
+  {
+    "revision": "9bc4d2843004cc19a95f1767a22ad19a",
+    "url": "/gwentReact-templateSait/static/media/preloader1920x1200.9bc4d284.jpg"
+  },
+  {
+    "revision": "7462a59a019f5f87c934844c1466f32b",
+    "url": "/gwentReact-templateSait/static/media/preloader800x480.7462a59a.jpg"
+  },
+  {
+    "revision": "fb88f1085399a3b6aa950a1797505263",
+    "url": "/gwentReact-templateSait/static/media/provision.fb88f108.png"
+  },
+  {
+    "revision": "8e4ff1e97d0a708d6772c836c679a78b",
+    "url": "/gwentReact-templateSait/static/media/secondWind.8e4ff1e9.png"
+  },
+  {
+    "revision": "8f1729c3e1441e6a920988969c028f61",
+    "url": "/gwentReact-templateSait/static/media/special.8f1729c3.png"
+  },
+  {
+    "revision": "d744e3fc1599e24f7ffff108846c67ef",
+    "url": "/gwentReact-templateSait/static/media/strategicWithdrawal.d744e3fc.png"
+  },
+  {
+    "revision": "f82b2a91fb4091051263765d890f42ba",
+    "url": "/gwentReact-templateSait/static/media/strategy.f82b2a91.png"
+  },
+  {
+    "revision": "7f18ba8aa94c9ef2fafc21c57ffdc1fb",
+    "url": "/gwentReact-templateSait/static/media/syndicate.7f18ba8a.png"
+  },
+  {
+    "revision": "d3f213ea18d3551baeeefa052831c6c8",
+    "url": "/gwentReact-templateSait/static/media/uprising.d3f213ea.png"
+  },
+  {
+    "revision": "5871c8901e0a5828d25c0cbcf445d1e0",
+    "url": "/gwentReact-templateSait/static/media/ursineRitual.5871c890.png"
+  },
+  {
+    "revision": "82f61d3e35aecdf7b1f4553131bccec7",
+    "url": "/gwentReact-templateSait/static/media/wildCard.82f61d3e.png"
+  }
+]);
